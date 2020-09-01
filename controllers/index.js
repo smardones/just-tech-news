26fc2e97ae6f28e345a20ea2d483d74cb63611bf
smardone <shaun.mardones@gmail.com>
2020-08-31 21:08:48 -0400

    set up MVC structure and created homepage html template

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes');
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
