e95c70b0e548b232f56a9d6d9494859287bb6a75
smardone <shaun.mardones@gmail.com>
2020-09-02 20:59:49 -0400

    added helper functions

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
     secret: 'Super secret secret',
